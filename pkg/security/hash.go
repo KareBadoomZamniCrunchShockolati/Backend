431ebf38adf05d4975a77d60c761dc016e591f51
Mobina Hoshiaripour <ma.hoshiar@gmail.com>
2025-10-09 00:57:53 +0330

    feat(auth): Implement Login/Signup handlers

---
+++ pkg/security/hash.go
@@ -0,0 +1,15 @@
+package security
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword generates a bcrypt hash of the password.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash compares a plaintext password with a hashed password.
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
